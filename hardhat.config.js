bab23d637c791c614d1ef0b79fe0a057c90c8c72
FranPandol <pandol.francisco@gmail.com>
2023-09-09 23:42:30 -0300

    Add hardhat config file

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+require('@nomiclabs/hardhat-ethers');
+const { alchemyApiKey, mnemonic, account } = require('./secrets.json');
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+     ropsten: {
+       url: alchemyApiKey,
+       accounts: [account,],
+       gas: "auto",
+     },
+  },
+};
